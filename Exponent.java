94ebc4b3caa6c8cbefc31db87cf0c684781d2c47
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-08 11:32:20 -0400

    exponent method

--- Exponent.java
+++ Exponent.java
@@ -5,5 +5,17 @@ class Exponent {
         // Input
         System.out.println("What's your x value?");
         int x = In.getInt();
+        exponent(x, x);
+    }
+
+    public static void exponent (double base, double power) {
+        
+        double output = 1;
+
+        for (int i = 0; i < power; i++) {
+                output *= base;
+            }
+
+        System.out.println(output);
     }
 }
