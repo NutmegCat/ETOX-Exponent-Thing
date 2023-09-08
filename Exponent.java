a26b7fb1850aed0277ccc50f88dd9aa689136533
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-08 10:39:34 -0400

    start

---
+++ Exponent.java
@@ -0,0 +1,9 @@
+// Aaron Prince Anu
+
+class Exponent {
+    public static void main(String[] args) {
+        // Input
+        System.out.println("What's your x value?");
+        int x = In.getInt();
+    }
+}
