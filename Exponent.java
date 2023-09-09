c028bbc2132b7cfb934442b863f9b9441afee2cc
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-08 20:50:35 -0400

    factorial

--- Exponent.java
+++ Exponent.java
@@ -6,6 +6,7 @@ class Exponent {
         System.out.println("What's your x value?");
         double x = In.getInt();
         exponent(x, x);
+        factorial(x);
     }
 
     public static void exponent (double base, double power) {
@@ -19,7 +20,18 @@ class Exponent {
         System.out.println(output);
     }
 
-    public static void factorial () {
+    public static void factorial (double factor) {
         // TODO take a number (n), multiply by (n - 1), then (n - 1) - 1, until 1
+
+        System.out.println(factor);
+
+        double finito = factor;
+        double fact = 1;
+
+        for (int i = 1; i <= finito; i++) {
+            fact = fact*i;
+        }
+
+        System.out.println(fact);
     }
 }
